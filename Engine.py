d3730ccea8202734a95979a54f6a1dd00a80b614
1MT3J45 <agenttj.rawal98@gmail.com>
2017-05-23 18:12:04 +0530

    Probability model for Recommendation

--- Engine.py
+++ Engine.py
@@ -30,6 +30,7 @@ items.head()
 r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
 ratings_base = pd.read_csv('ml-100k/ua.base', sep='\t', names=r_cols, encoding='latin-1')
 ratings_test = pd.read_csv('ml-100k/ua.test', sep='\t', names=r_cols, encoding='latin-1')
+movies = pd.read_csv('ml-100k/u.info', sep='|')
 
 print(ratings_base.shape)
 print(ratings_base)
@@ -40,3 +41,9 @@ train_data = graphlab.SFrame(ratings_base)
 test_data = graphlab.SFrame(ratings_test)
 
 popularity_model = graphlab.popularity_recommender.create(train_data, user_id='user_id', item_id='movie_id', target='rating')
+
+# Get recommendations for first 5 users and print them
+# users = range(1,6) specifies user ID of first 5 users
+# k=5 specifies top 5 recommendations to be given
+popularity_recomm = popularity_model.recommend(users=range(112,220),k=5)
+popularity_recomm.print_rows(num_rows=31)
