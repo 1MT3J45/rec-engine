0a609c3a26993c5f626676a4d4aa4508b834e752
1MT3J45 <agenttj.rawal98@gmail.com>
2017-05-24 20:06:31 +0530

    T&T Data gen, CSV Exported

--- Engine.py
+++ Engine.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import graphlab
+import csv
 
 # pass in column names for each CSV and read them using pandas.
 # Column names available in the readme file
@@ -30,22 +31,49 @@ items.head()
 r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
 ratings_base = pd.read_csv('ml-100k/ua.base', sep='\t', names=r_cols, encoding='latin-1')
 ratings_test = pd.read_csv('ml-100k/ua.test', sep='\t', names=r_cols, encoding='latin-1')
-movies = pd.read_csv('ml-100k/u.info', sep='|')
+info = pd.read_csv('ml-100k/u.info', sep='|')
 
+print ("________ RATING SIZE __________")
 print(ratings_base.shape)
+
+print ("________ RATING BASE __________")
 print(ratings_base)
+
+print ("________ RATING TEST __________")
 print(ratings_test)
 
+print ("________ GEN. INFO __________")
+print(info)
 
 train_data = graphlab.SFrame(ratings_base)
 test_data = graphlab.SFrame(ratings_test)
 
-popularity_model = graphlab.popularity_recommender.create(train_data, user_id='user_id', item_id='movie_id', target='rating')
+print ("________ TRAIN DATA __________")
+print (train_data)
 
-# Get recommendations for first 5 users and print them
-# users = range(1,6) specifies user ID of first 5 users
-# k=5 specifies top 5 recommendations to be given
-popularity_recomm = popularity_model.recommend(users=range(112,220),k=5)
-popularity_recomm.print_rows(num_rows=31)
+df1 = train_data.to_dataframe()
+df1.to_csv("train_data.csv", sep=",")
 
-pd.read_csv()
+#with open('train_data.csv','wb') as myfile:
+#    wr = csv.writer(myfile)
+#    wr.writerow(train_data)
+#myfile.close()
+#-----------------------REPLACING MOVIE ID WITH MOVIE NAMES-----#
+
+f = open('ml-100k/u.item', 'rw')
+fo = open('train_data.csv', 'rw')
+
+# go through each line of the file
+#for line in f:
+#    bits = line.split('|')
+    # change second column
+#    bits[1] = 'name'
+    # join it back together and write it out
+#    fo.write(','.join(bits))
+#print (f.read())
+
+f.close()
+fo.close()
+#x = open("out.csv")
+#y = x.read()
+#print (y)
--- test.py
+++ test.py
@@ -1,29 +1,61 @@
 import pandas as pd
-import sys
+import os
 import csv
 
+f = open('mycsv.csv','rb')
+fo = open('out.csv','wb')
+
+# go through each line of the file
+for line in f:
+    bits = line.split(',')
+    # change second column
+    bits[1] = '"NAME_UNDEFINED"'
+    # join it back together and write it out
+    fo.write( ','.join(bits) )
+
+
+
+f.close()
+fo.close()
+x=open("out.csv")
+y=x.read()
+print (y)
+
+# _______________________TRIAL TWO_____________________
+#normalize = lambda x: "%.4f" % float(X)
+
+# df1 = pd.read_csv("ml-100k/u.item", index_col=(0, 1), usecols=(1,2), header=None, converters=dict.fromkeys([1, 2], normalize))
+# df2 = pd.read_csv("ml-100k/u.item", index_col=(0, 1), usecols=(1,2), header=None, converters=dict.fromkeys([1, 2], normalize))
+
+# result = df1.join(df2, how='inner')
+# result.to_csv("output.csv", header=None)
+# _______________________TRIAL ONE_____________________
 # Reading users file:
 # u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
 # users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols, encoding='latin-1')
 
-try:
-    reader = pd.read_csv('ml-100k/u.user',delimiter='|',usecols=[0,1,2,4],nrows=3)
-    print(type(reader),"\n\n")
+# try:
+#    reader = pd.read_csv('ml-100k/u.item', delimiter='|', usecols=[0, 1, 2, 4], nrows=3)
+#    print ("\n")
+#    print(reader)
+#    print (range(5))
+#    # col = int(input("Input number column to print"))
+#    row = int(input("Input number of rows to print"))
+#    list1 = [0, 0, 0, 0]
+#    for i in range(3):
+#        list1[i] = input("Insert val")
+#        print (i)
+#        i += 1
+#    print (pd.read_csv('ml-100k/u.item', delimiter='|', usecols=list1, nrows=row))
+#    # match_list = pd.read_csv(pd.read_csv('ml-100k/u.item', delimiter='|', usecols=list1, nrows=row))
+#    # print ("\n THIS IS NEW MATCH LIST", match_list)
 
-    print(reader)
-    print (range(5))
-    # col = int(input("Input number column to print"))
-    row = int(input("Input number of rows to print"))
-    list1=[0,0,0,0]
-
-    for i in range(3):
-        list1[i] = input("Insert val")
-        print (i)
-        i += 1
-
-
-    print (pd.read_csv('ml-100k/u.user',delimiter='|',usecols=list1,nrows=row))
-    match_list = pd.read_csv(pd.read_csv('ml-100k/u.user',delimiter='|',usecols=list1,nrows=row))
-    print ("\n THIS IS NEW MATCH LIST",match_list)
-finally:
-    exit(0)
+#    rec_list=()
+#    records=int(input("How many records to collect?\n"))
+#    for i in range(records):
+#        abc = (pd.read_csv(pd.read_csv('ml-100k/u.item', delimiter='|', usecols=[0], nrows=records)))
+#        i +=1
+#    # rec_list = ("Sr",1,2,3)
+#        print (abc)
+# finally:
+#    exit(0)
