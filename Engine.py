7d3bfb2b3b5daa1d4ebcf7e9b86c78e64d1a508f
1MT3J45 <agenttj.rawal98@gmail.com>
2017-06-04 19:31:26 +0530

    Running with Cosine and Jaccard (Change parameter of Item similarity)

--- Engine.py
+++ Engine.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import graphlab
-import csv
+import time
 
 # pass in column names for each CSV and read them using pandas.
 # Column names available in the readme file
@@ -31,10 +31,10 @@ items.head()       # Not Printing only Headers than entire data
 
 # Fetching DB of Ratings per Movie for every user
 r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
-ratings_base = pd.read_csv('ml-100k/ua.base', sep='\t', names=r_cols, encoding='latin-1')
+ratings_base = pd.read_csv('ml-100k/u2.base', sep='\t', names=r_cols, encoding='latin-1')
 
 # Fetching DB of Testing Data for Movies Users have Rated
-ratings_test = pd.read_csv('ml-100k/ua.test', sep='\t', names=r_cols, encoding='latin-1')
+ratings_test = pd.read_csv('ml-100k/u2.test', sep='\t', names=r_cols, encoding='latin-1')
 info = pd.read_csv('ml-100k/u.info', sep='|')
 
 print ("________ RATING SIZE __________")
@@ -72,20 +72,26 @@ print(ratings_base.groupby(by='movie_id')['rating'].mean().sort_values(ascending
 # Preparing an ITEM Similarity model
 
 # Train Model
-item_sim_model = graphlab.item_similarity_recommender.create(train_data, user_id='user_id', item_id='movie_id', target='rating', similarity_type='pearson')
+item_sim_model = graphlab.item_similarity_recommender.create(train_data, user_id='user_id', item_id='movie_id',
+                                                             target='rating', similarity_type='cosine')
 # Make Recommendations
 item_sim_recomm = item_sim_model.recommend(users=range(1,6),k=5)
-item_sim_recomm.print_rows(num_rows=20)
+item_sim_recomm.print_rows(num_rows=25)
+
+print("-----------------------> ", type(item_sim_recomm))
 
 # <editor-fold desc="Printing Recommendation with Movie names but not limited to Rank">
-# df_item_sim = item_sim_recomm.to_dataframe()        # DF2 Recom | user_id | movie_id | score | rank |
-# df_movies = pd.read_csv('ml-100k/u.item',sep='|')   # DF3 Items | ID | MOVIE NAME (YEAR)| REL.DATE | NULL | IMDB LINK|
+df_item_sim = item_sim_recomm.to_dataframe()  # DF2 Recom | user_id | movie_id | score | rank |
+df_movies = pd.read_csv('ml-100k/u.item', sep='|')  # DF3 Items | ID | MOVIE NAME (YEAR)| REL.DATE | NULL | IMDB LINK|
 
-# df_item_sim['movie_id'] = df_item_sim['movie_id'].replace(df_movies.set_index('ID')['MOVIE NAME (YEAR)'])
-# print ("________ REC W/ MOVIE NAMES ___________")
-# print (df_item_sim)
+df_item_sim['movie names'] = df_item_sim['movie_id'].replace(df_movies.set_index('ID')['MOVIE NAME (YEAR)'])
+print ("________ REC W/ MOVIE NAMES ___________")
+print (df_item_sim)
 # </editor-fold>
 
+model_performance = graphlab.compare(test_data, [popularity_model, item_sim_model])
+graphlab.show_comparison(model_performance, [popularity_model, item_sim_model])
+time.sleep(180)
 # print ("***********_______TO BE USED LATER________************")
 # df1 = train_data.to_dataframe()
 # df1.to_csv("train_data.csv", sep=",")
