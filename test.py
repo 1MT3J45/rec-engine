aa38e41e13be7f78bd49c517490d44cdc37bfc60
1MT3J45 <agenttj.rawal98@gmail.com>
2017-06-07 10:35:41 +0530

    Separate Algo. Unit

--- test.py
+++ test.py
@@ -2,55 +2,14 @@ import pandas as pd
 import graphlab
 import time
 
-# pass in column names for each CSV and read them using pandas.
-# Column names available in the readme file
+train_data = pd.read_pickle('train_data.pickle')
+test_data = pd.read_pickle('test_data.pickle')
+popularity_model = ('pop_model')
 
-# Reading users file:
-u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
-users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols, encoding='latin-1')
-
-# Reading ratings file:
-r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
-ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols, encoding='latin-1')
-
-# Reading items file:
-i_cols = ['movie id', 'movie title', 'release date', 'video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
-          'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 'Horror',
-          'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
-items = pd.read_csv('ml-100k/u.item', sep='|', names=i_cols, encoding='latin-1')
-
-print users.shape
-users.head()
-
-print ratings.shape
-ratings.head()
-
-print items.shape
-items.head()
-
-r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
-ratings_base = pd.read_csv('ml-100k/u5.base', sep='\t', names=r_cols, encoding='latin-1')
-ratings_test = pd.read_csv('ml-100k/u5.test', sep='\t', names=r_cols, encoding='latin-1')
-print (ratings_base.shape)
-print (ratings_test.shape)
-
-train_data = graphlab.SFrame(ratings_base)
-test_data = graphlab.SFrame(ratings_test)
-
-popularity_model = graphlab.popularity_recommender.create(train_data, user_id='user_id', item_id='movie_id',
-                                                          target='rating')
-
-# Get recommendations for first 5 users and print them
-# users = range(1,6) specifies user ID of first 5 users
-# k=5 specifies top 5 recommendations to be given
-popularity_recomm = popularity_model.recommend(users=range(1, 6), k=5)
-popularity_recomm.print_rows(num_rows=25)
-
-ratings_base.groupby(by='movie_id')['rating'].mean().sort_values(ascending=False).head(20)
+algo = raw_input("COSINE | JACCARD | PEARSON \n Enter the name of algo:")
 
 # Train Model
-item_sim_model = graphlab.item_similarity_recommender.create(train_data, user_id='user_id', item_id='movie_id',
-                                                             target='rating', similarity_type='cosine')
+item_sim_model = graphlab.item_similarity_recommender.create(train_data, user_id='user_id', item_id='movie_id', target='rating', similarity_type=algo)
 
 # Make Recommendations:
 item_sim_recomm = item_sim_model.recommend(users=range(1, 6), k=5)
