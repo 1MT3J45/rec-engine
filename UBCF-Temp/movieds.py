d3da3f6549cd88672e736d0a472ef52c9f0c0c47
1MT3J45 <agenttj.rawal98@gmail.com>
2017-06-14 20:06:03 +0530

    Mini-Dataset for Testing only

---
+++ UBCF-Temp/movieds.py
@@ -0,0 +1,43 @@
+# Data set of people and movies they have rate
+# 0 means either they haven't watched it yet.
+# The last movie is appended with a number to carry out visual analysis manually
+
+dataset={
+			'Tejas': {'Fast and Furious': 5,
+							'Dr. Strange': 5,
+							'Shawshank Redemption': 5,
+							'Angry Indian Godesses': 0,
+							'The Lunchbox':0,
+					  	    'Baby1':0
+							},
+
+			'Niket': {'Fast and Furious': 0,
+							'Dr. Strange': 0,
+							'Shawshank Redemption': 5,
+							 'Angry Indian Godesses': 4,
+							  'The Lunchbox':4,
+					  		  'Baby2':0
+							 },
+
+			'Siddhi': {'Fast and Furious': 0,
+								'Dr. Strange': 0,
+								'Shawshank Redemption': 0,
+								 'Angry Indian Godesses': 5,
+								 'The Lunchbox':4,
+					  			 'Baby3':3
+								 },
+
+			'Smital': {'Fast and Furious': 5,
+							'Dr. Strange': 5,
+							'Shawshank Redemption': 0,
+							'Angry Indian Godesses': 0,
+							'The Lunchbox': 5,
+							'Baby4':5},
+
+            'Sanket': {'Fast and Furious': 5,
+							'Dr. Strange': 5,
+							'Shawshank Redemption': 0,
+							'Angry Indian Godesses': 5,
+							'The Lunchbox': 4,
+							'Baby4':3}
+}
