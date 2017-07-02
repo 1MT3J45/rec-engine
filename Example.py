7a7a7d064e30d8140c905ef6c44448fbd88ff001
1mt3j45 <tejasprawal@gmail.com>
2017-07-02 19:12:57 +0530

    Calculating RMSE, MAE

--- Example.py
+++ Example.py
@@ -1,9 +1,8 @@
-import sys
-import recsys.prediction_algorithms
-import recsys.dataset as data1
+from sklearn.model_selection import KFold, cross_val_predict, cross_val_score
+from surprise import Reader, Dataset, evaluate, print_perf, SVD
+import recsys
+import numpy as np
 import pandas as pd
-import csv
-import surprise
 
 # KNN = recsys.KNNBasic(40,1,{'name':'cosine','user_based':True})
 # print KNN.predict()
@@ -11,19 +10,35 @@ headers = ['UserID', 'MovieID', 'Rating', 'TimeStamp']
 
 df = pd.read_csv('ml-100k/u.data', sep='\t', names=('UserID', 'MovieID', 'Rating', 'TimeStamp'))
 print df.dtypes, '\n', '____________________'
-print df
 
+
+# Printing only selected columns
+values = df.values
+print values[:,0:2]
+
+# Simple sorting of values
 df2 = df.sort_values(['UserID','MovieID'])
 print type(df2)
-df2.to_csv("sorted.csv", sep="\t")
 
 print("*___________________*")
-df3 = df.pivot_table(values='Rating',index='UserID',columns='MovieID', aggfunc='mean')
+df3 = df.pivot_table(values='Rating',index='UserID',columns='MovieID')
+## FOLL STATEMENTS PRODUCE THE SAME RESULT
+# df3 = df.set_index(['UserID','MovieID'])['Rating'].unstack()
+# df3 = df.pivot(index='UserID',columns='MovieID',values='Rating')
+# df3 = df.groupby(['UserID','MovieID'])['Rating'].mean().unstack()
 print df3
-df3.to_csv("matrix.csv",sep="\t")
-#print type(data)
-#print df
-#print pd.melt(data_f,id_vars='MovieID')
-#print (data.set_index('UserID').T)
-#print (df.set_index('UserID').T)
-#print (df.rename(columns={'UserID': 'Users 1 ID'}).set_index(None).rename_axis().T)
+
+reader = Reader(line_format="user item rating", sep='\t', rating_scale=(1,5))
+df = Dataset.load_from_file('ml-100k/u.data', reader=reader)
+df.split(n_folds=5)
+
+perf = evaluate(SVD(),df,measures=['RMSE','MAE'])
+print_perf(perf)
+
+#predicted = cross_val_predict(X=df,y=None,cv=5)
+#print predicted
+
+# kf = KFold(n_splits=4)
+# for i in kf.split(df):
+#    print ("%d" % (i))
+
